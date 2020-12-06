e07a3614e58e2d7d1e0ff522911f7848a11063f7
Miha Jakovac <miha@mjc.si>
2020-12-06 12:27:19 +0100

    updated tests

---
+++ src/QAToolKit.Auth.Test/Keycloak/KeycloakAuthenticatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NSubstitute;
+using QAToolKit.Core.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QAToolKit.Auth.Test.Keycloak
+{
+    public class KeycloakAuthenticatorTests
+    {
+        [Fact]
+        public async Task CreateAuthenticatonServiceTest_Success()
+        {
+            var authenticator = Substitute.For<IAuthenticationService>();
+            await authenticator.GetAccessToken();
+            Assert.Single(authenticator.ReceivedCalls());
+        }
+
+        [Fact]
+        public async Task CreateAuthenticatonServiceWithReturnsTest_Success()
+        {
+            var authenticator = Substitute.For<IAuthenticationService>();
+            authenticator.GetAccessToken().Returns(args => "12345");
+
+            Assert.Equal("12345", await authenticator.GetAccessToken());
+            Assert.Single(authenticator.ReceivedCalls());
+        }
+
+        [Fact]
+        public void CreateKeycloakOptionsTest_Success()
+        {
+            var options = new KeycloakOptions();
+            options.AddClientCredentialFlowParameters(new Uri("https://api.com/token"), "12345", "12345");
+            options.AddUserNameForImpersonation("myemail@email.com");
+
+            var keycloakOptions = Substitute.For<Action<KeycloakOptions>>();
+            keycloakOptions.Invoke(options);
+            Assert.Single(keycloakOptions.ReceivedCalls());
+        }
+    }
+}
